57cfd014a38f56c7757357624a6f715d9e65caef
wjy491156866 <48510280+wjy491156866@users.noreply.github.com>
2019-03-27 21:01:49 +0800

    丑数

---
+++ Simple/isUgly.cpp
@@ -0,0 +1,23 @@
+//263. 丑数
+//编写一个程序判断给定的数是否为丑数。
+//丑数就是只包含质因数 2, 3, 5 的正整数。
+
+class Solution {
+public:
+    bool isUgly(int num) {
+        if (num < 1) return false;
+        if (num >= 1)
+        {
+            while (num % 2 == 0)
+                num /= 2;
+            while (num % 5 == 0)
+                num /= 5;
+            while (num % 3 == 0)
+                num /= 3;
+        }
+        if (num == 1)
+                return true;
+            else 
+                return false;
+    }
+};
