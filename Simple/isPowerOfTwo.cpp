0f6280bacf60535658e043044f45046fe5155e95
wjy491156866 <48510280+wjy491156866@users.noreply.github.com>
2019-05-19 00:25:36 +0800

    2的幂

---
+++ Simple/isPowerOfTwo.cpp
@@ -0,0 +1,18 @@
+//231. 2的幂
+//给定一个整数，编写一个函数来判断它是否是 2 的幂次方。
+
+
+//方法一：连除法
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n == 1) return true;  //如果是1正确
+        if (n%2 == 1 || n <= 0) return false; //如果小于或等于0，或者不能整除2为错误
+        while (true)
+        {
+            n = n/2;  //得到除以2之后的数
+            if(n%2 && n == 1) return true;
+            if(n%2 && n != 1) return false;
+        }
+    }
+};
