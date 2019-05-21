c10d89b59c073ea9684d50244a474eab38fa1231
wjy491156866 <48510280+wjy491156866@users.noreply.github.com>
2019-05-21 18:00:43 +0800

    最小栈——添加方法二

--- Simple/MinStack.cpp
+++ Simple/MinStack.cpp
@@ -59,3 +59,71 @@ public:
  * int param_3 = obj->top();
  * int param_4 = obj->getMin();
  */
+
+
+
+
+
+//方法二：数组法
+//在做pop操作时，可能需要更新最小元素所在的位置，会有一次O(n)的遍历。
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    vector<int> nums;       //定义数组
+    int min_index = 0;        //最小值索引
+    
+    MinStack() {        //留空
+        
+    }
+    
+    void push(int x) {      //把x加入栈顶
+        nums.push_back(x);      //加入数组的最后
+        if(x < nums[min_index])    //如果x小于当前数组的最小值 
+        {
+            min_index = nums.size() - 1;        //最小值索引改变
+        }
+    }
+    
+    void pop() {        //删除栈顶元素
+        if(nums.size() == 0) cout<<"Empty stack";       //空栈提示
+        else
+        {
+            nums.pop_back();        //把数组最后的数删掉
+            if(min_index != nums.size()) ;      //如果最小数正好是数组最后一位，就不更新索引
+            else        //如果不是最后一位，需要找到最小值然后重新更新索引
+            {
+                int min = nums[0];
+                min_index = 0;
+                for(int i=0; i<nums.size(); i++)
+                {
+                    if(min > nums[i])
+                    {
+                        min = nums[i];
+                        min_index = i;      //更新索引
+                    }
+                    
+                }
+            }
+                
+        }
+    }
+    
+    int top() {     //获取栈顶元素
+        if(nums.size() == 0) return -1;     //空栈提示
+        else return nums.back();        //返回数组最后一个数
+    }
+    
+    int getMin() {      //检索栈中的最小元素
+        if(nums.size() == 0) return -1;     //空栈提示
+        else return nums[min_index];       //返回最小值
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
