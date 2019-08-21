0d80d30aee1258c81754ae10cbee1ccf5f6e4ba2
wjy491156866 <48510280+wjy491156866@users.noreply.github.com>
2019-08-21 10:39:02 +0800

    反转链表

--- Simple/reverseList.cpp
+++ Simple/reverseList.cpp
@@ -14,16 +14,16 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode prev = NULL;
-        ListNode *curr = head;
-        while (curr != NULL)
+        ListNode prev = NULL;       //新建空节点
+        ListNode *curr = head;      //新建指针指向头节点
+        while (curr != NULL)        //当当前节点不是NULL时
         {
-            ListNode *nexttemp = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = nexttemp;
+            ListNode *nexttemp = curr->next;        //保存当前节点的next
+            curr->next = prev;      //当前节点的next改变方向
+            prev = curr;        //前一节点指针后移
+            curr = nexttemp;        //当前节点指针后移
         }
-        return prev;
+        return prev;        //最后指向头
     }
 };
 
