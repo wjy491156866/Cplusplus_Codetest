48e07139595a8e0ef82f66c2d5471e57e8e75bc4
wjy491156866 <48510280+wjy491156866@users.noreply.github.com>
2019-09-08 16:50:20 +0800

    阶乘后的零

---
+++ Simple/trailingZeroes.cpp
@@ -0,0 +1,15 @@
+//172. 阶乘后的零
+//给定一个整数 n，返回 n! 结果尾数中零的数量。
+
+//计算5的个数
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int count=0;
+        while(n>=5){
+            count += n/5;
+            n /= 5;
+        }
+        return count;
+    }
+};
