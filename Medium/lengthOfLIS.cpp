0a0369ecbb9b7ad8b6e7730891ba35b1aa7adff2
wjy491156866 <48510280+wjy491156866@users.noreply.github.com>
2019-10-04 20:55:15 +0800

    300. 最长上升子序列

--- Medium/lengthOfLIS.cpp
+++ Medium/lengthOfLIS.cpp
@@ -1,7 +1,39 @@
 //300. 最长上升子序列
 //给定一个无序的整数数组，找到其中最长上升子序列的长度。
 
+//方法一：动态规划贪心算法，找最小的与之前的替换。
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int len = nums.size(), count = 0;
+        if(len<2)   return len;
+        vector<int> res;
+        res.push_back(nums[0]);
+        for(int i=1; i<len; i++){
+            if(nums[i]>res[count]){
+                res.push_back(nums[i]);
+                count++;
+            }
+            else{	// 35题 搜索插入的位置 二分搜索
+                int left=0, right=count;
+                while(left<right){
+                    int mid=(left+right)>>1;        //相当于/2(无符号整型可以这么做)
+                    if(res[mid]>=nums[i]){
+                        right=mid;
+                    }
+                    else{
+                        left=mid+1;
+                    }
+                }
+                res[left]=nums[i];	//左边界为插入位置，直接替换
+            }
+        }
+        return count+1;
+    }
+};
 
+
+//方法二：lower_bound函数法
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
