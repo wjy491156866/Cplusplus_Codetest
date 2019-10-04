f50fc77ba705b313607b8010e378fa635df99d97
wjy491156866 <48510280+wjy491156866@users.noreply.github.com>
2019-10-04 16:04:27 +0800

    300. 最长上升子序列

--- Medium/lengthOfLIS.cpp
+++ Medium/lengthOfLIS.cpp
@@ -7,19 +7,19 @@ public:
     int lengthOfLIS(vector<int>& nums) {
         int len = nums.size();
         if(len<=1)  return len;
-		vector<int> res;
-		for (int i = 0; i < len; ++i) {
-			auto iter = lower_bound(res.begin(), res.end(), nums[i]);//寻找第一个不小于nums[i]的元素
+	vector<int> res;
+	for (int i = 0; i < len; ++i) {
+		auto iter = lower_bound(res.begin(), res.end(), nums[i]);//寻找第一个不小于nums[i]的元素
 			//只有在res后面放入元素才会更改结果
-			if (iter == res.end()) {//如果是end，说明res中所有元素都小于nums[i],这个元素直接放入
-				res.push_back(nums[i]);
-			}
-			else {
-				//修改操作不会影响结果，把这个大的元素修改为小的元素是为了后面可能出现更长的上升子序列
-				*iter = nums[i]; //更新
-			}
+		if (iter == res.end()) {//如果是end，说明res中所有元素都小于nums[i],这个元素直接放入
+			res.push_back(nums[i]);
 		}
-		return res.size();
+		else {
+			//修改操作不会影响结果，把这个大的元素修改为小的元素是为了后面可能出现更长的上升子序列
+			*iter = nums[i]; //更新
+		}
+	}
+	return res.size();
     }
 };
 
